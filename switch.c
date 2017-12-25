edc5fd226e06c1d05914acb9562ce03742781fb1
howpwn <finn79426@gmail.com>
2017-12-25 17:35:00 +0800

    [Done] 實驗switch的使用方法

---
+++ switch.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	char grade;
+
+	printf("Input your grade (A-E) :");
+	scanf("%c",&grade);
+
+	if ((grade >= 'A') && (grade <= 'Z')){
+		grade = 'a' + (grade - 'A');
+	}
+
+	switch (grade){
+		case 'a' : printf("Score 90 to 100. \n");
+				   break;
+		case 'b' : printf("Score 80 to 89. \n");
+				   break;
+		case 'c' : printf("Score 70 to 79. \n");
+				   break;
+		case 'd' : printf("Score 60 to 69. \n");
+				   break;
+		case 'e' : printf("Score under 60. \n");
+				   break;
+		default:   printf("Wrong grade! \n");
+	}
+
+	return 0;
+}
