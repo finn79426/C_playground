685cea461b7b97d91cee2446718a3eb25230c780
howpwn <finn79426@gmail.com>
2017-12-25 17:51:56 +0800

    [Done] 用for迴圈畫出聖誕樹(另一種演算法)

---
+++ c_class/christmas_tree_V2.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int i;
+	int init_num;
+
+	for(i=0;i<6;i++){
+		init_num = init_num * 10 + 5;
+		printf("%d\n",init_num);
+	}
+
+	/*
+	 
+	關鍵：
+	第1行印1個
+	第2行印2個
+	第3行印3個
+	以此類推
+	
+	*/
+	 		 	
+	return 0;
+}
