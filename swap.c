140c7856883d366cd3846b7099f2b3d472d2afb7
howpwn <finn79426@gmail.com>
2017-12-25 17:32:58 +0800

    [Done] 實驗pass by address

---
+++ swap.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+void swap(int *, int *);
+int main(){
+	int x,y;
+
+	x = 14;
+	y = 62;
+
+	printf("Initial ... \n");
+	printf("	x = %d y = %d",x,y);
+	swap(&x, &y);
+	printf("\nAfter sqpping ...\n");
+	printf("	x = %d y = %d\n",x,y);
+
+	return 0;
+}
+
+void swap(int *a, int *b){
+	int temp;
+	printf("\nIn swap() ... \n");
+	printf("	*a = %d	*b = %d\n",*a,*b);
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	printf("End swap() ....\n");
+	printf("	*a = %d *b = %d\n",*a,*b);	
+}
