70b3f475e946db3f0122aafea10b03c55faf8201
howpwn <finn79426@gmail.com>
2018-11-19 10:48:18 +0800

    資料結構作業備份

---
+++ 1112DS陣列.c
@@ -0,0 +1,54 @@
+/*
+ * By : howpwn
+ * Email : finn79426@gmail.com
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	int array[101];
+	int newNumber, index;
+	for(int i=0; i<100; i++){
+		/*printf("Data of Array[%d] : ", i+1);*/
+		/*scanf("%d", &array[i]);*/
+		int random = (rand()%100)+1;
+		array[i] = random;
+	}
+
+	// Output origin
+	for(int i=0; i<100; i++){
+		printf("%d\t", array[i]);
+	}
+	printf("\n");
+
+	// Input
+	printf("Input your number : ");
+	scanf("%d", &newNumber);
+	index = newNumber % 10;
+	
+	clock_t begin = clock();
+
+	// Insert
+	for(int i=11-1; i>=index-1; i--){
+		array[i+1] = array[i];
+	}
+	array[index] = newNumber;
+
+	clock_t end = clock();
+
+
+	// Output result
+	for(int i=0; i<100; i++){
+		printf("%d\t", array[i]);
+	}
+	printf("\n");
+	
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Alogrithm used time: %f sec\n", time_spent);	
+
+
+	return 0;
+}
+
